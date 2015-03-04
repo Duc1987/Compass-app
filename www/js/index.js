1e3f9637ec306e86ffe41881f883b8a5444d514f
Duc <n.duc1987@gmail.com>
2015-03-04 12:37:46 +0100

    Using phonegap API's

---
+++ www/js/index.js
@@ -0,0 +1,45 @@
+// Wait for device API libraries to load
+//
+
+// Opstarten
+document.addEventListener("deviceready", deviceReady, false);
+
+// device APIs are available
+//
+function deviceReady() {
+   
+    var options = { frequency: 10 };    
+    navigator.accelerometer.watchAcceleration(accelSuccess, accelError, options); 
+    navigator.compass.watchHeading(compassSuccess, compassError, options);
+}
+
+//ACCELERATION
+function accelSuccess(acceleration) {
+    var yDegree = acceleration.y * 6;
+    $('#country').css({
+        '-webkit-transform' : 'skewY(' + yDegree + 'deg)',
+        '-moz-transform' : 'skewY(' + yDegree + 'deg)',
+        'transform' : 'skewY(' + yDegree + 'deg)'
+    });
+}
+function accelError() { }
+
+//COMPASS
+function compassSuccess(heading) {
+
+ 
+    var countries = [
+        { country: 'sweden', direction: 40 },
+        { country: 'china', direction: 115 },
+        { country: 'south-africa', direction: 160 },
+        { country: 'america', direction: 250 }
+    ];
+   
+    for (var i = 0; i < countries.length; i++) {
+        if(heading.magneticHeading >= countries[i].direction) {
+            $('#country').attr('src', 'img/' + countries[i].country + '.GIF');
+        }
+    };
+
+}
+function compassError() { }
